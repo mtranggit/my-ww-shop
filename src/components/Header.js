c2c1b4633e20c56fd3da2d736e73a117497e9669
Michael Trang <michael.trang@hotmail.com>
2021-01-23 11:32:33 +1100

    Add React Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,27 @@
 import {Container, Nav, Navbar} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 export const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">My WW Shop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand href="/">My WW Shop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">
-                <i className="fa fa-shopping-cart"></i> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className="fa fa-user"></i> Login
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fa fa-shopping-cart"></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fa fa-user"></i> Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,9 @@
+import {Container} from 'react-bootstrap'
+
+export const ProductScreen = () => {
+  return (
+    <>
+      <Container>Product</Container>
+    </>
+  )
+}
